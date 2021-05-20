3aca8b2c1cffc6dd9bb35d88d117dcd837719300
Marius Grossu <mariustingr@gmail.com>
2021-05-20 21:14:30 +0200

    project 2 AutoFocus

---
+++ AutoFocus/AutoFocus/ViewController.swift
@@ -0,0 +1,114 @@
+//
+//  ViewController.swift
+//  AutoFocus
+//
+//  Created by Marius Grossu on 19.05.2021.
+//
+
+import UIKit
+import AVFoundation
+import Vision
+
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    //variables
+    
+    private let captureSession = AVCaptureSession()
+    //lazy because first the captureSession needs to be loaded
+    private lazy var previewLayer: AVCaptureVideoPreviewLayer = {
+        let preview = AVCaptureVideoPreviewLayer(session: self.captureSession)
+        preview.videoGravity = .resizeAspectFill
+        return preview
+    }()
+    private let videoOutput = AVCaptureVideoDataOutput()
+    private var drawings: [CAShapeLayer] = []
+
+    //functions
+    
+    //add default rear camera feed
+    private func addCameraInput(){
+        let device = AVCaptureDevice.default(for: .video)!
+        let cameraInput = try! AVCaptureDeviceInput(device: device)
+        self.captureSession.addInput(cameraInput)
+    }
+    
+    //display the camera feed
+    private func addPreviewLayer() {
+        self.view.layer.addSublayer(self.previewLayer)
+    }
+    
+    //send live camera frames to ViewController
+    private func addVideoOutput() {
+        self.videoOutput.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as NSString) : NSNumber(value: kCVPixelFormatType_32BGRA)] as [String : Any]
+        self.videoOutput.alwaysDiscardsLateVideoFrames = true
+        self.videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "camera_frame_processing_queue"))
+        self.captureSession.addOutput(self.videoOutput)
+        
+        guard let connection = self.videoOutput.connection(with: AVMediaType.video),
+                connection.isVideoOrientationSupported else { return }
+            connection.videoOrientation = .portrait
+    }
+    
+    //focus on the face detected
+    private func handleFaceDetectionResults(_ observedFaces: [VNFaceObservation]) {
+        self.clearDrawings()
+        let facesBoundingBoxes: [CAShapeLayer] = observedFaces.map({ (observedFace: VNFaceObservation) -> CAShapeLayer in
+            let faceBoundingBoxOnScreen = self.previewLayer.layerRectConverted(fromMetadataOutputRect: observedFace.boundingBox)
+            let faceBoundingBoxPath = CGPath(rect: faceBoundingBoxOnScreen, transform: nil)
+            let faceBoundingBoxShape = CAShapeLayer()
+            faceBoundingBoxShape.path = faceBoundingBoxPath
+            faceBoundingBoxShape.fillColor = UIColor.clear.cgColor
+            faceBoundingBoxShape.strokeColor = UIColor.green.cgColor
+            return faceBoundingBoxShape
+        })
+        facesBoundingBoxes.forEach({ faceBoundingBox in self.view.layer.addSublayer(faceBoundingBox) })
+        self.drawings = facesBoundingBoxes
+    }
+    private func clearDrawings() {
+        self.drawings.forEach({ drawing in drawing.removeFromSuperlayer() })
+    }
+    
+    //make the request to detect the face, handles the request
+    private func detectFace(in image: CVPixelBuffer) {
+        let faceDetectionRequest = VNDetectFaceLandmarksRequest(completionHandler: { (request: VNRequest, error: Error?) in
+            DispatchQueue.main.async {
+                if let results = request.results as? [VNFaceObservation] {
+                    self.handleFaceDetectionResults(results)
+                } else {
+                    self.clearDrawings()
+                }
+            }
+        })
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: image, orientation: .leftMirrored, options: [:])
+        try? imageRequestHandler.perform([faceDetectionRequest])
+    }
+    
+    //receives the frames
+    func captureOutput(_ output: AVCaptureOutput,
+                       didOutput sampleBuffer: CMSampleBuffer,
+                       from connection: AVCaptureConnection) {
+        guard let frame = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            debugPrint("unable to get image from sample buffer")
+            return
+        }
+        //print("did receive image frame")
+        // process image here
+        self.detectFace(in: frame)
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.addCameraInput()
+        self.addPreviewLayer()
+        self.addVideoOutput()
+        self.captureSession.startRunning()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.previewLayer.frame = self.view.frame
+    }
+
+
+}
--- Project1/Sources/Simulator/Simulator.swift
+++ Project1/Sources/Simulator/Simulator.swift
@@ -25,34 +25,44 @@ public struct Simulator {
     public func simulate(on string: String) -> [String] {
 
       let symbol_array = string.split(separator: ",")
-      var current_state = finiteAutomata.initialState
-      var states = [current_state]
+      let initial_state = finiteAutomata.initialState
+      var current_state = [initial_state]
+
 
       for current_symbol in symbol_array {
-        let new_state = check_transitions(state: current_state, symbol: String(current_symbol))
-        if new_state != "" {
-          states.append(new_state)
-          current_state = new_state
-        } else {
-          return []
+        var next : [String] = []
+        for state in current_state{
+            for transition in finiteAutomata.transitions{
+                if transition.from == state && transition.with == current_symbol{
+                    next.append(transition.to)
+                }
+            }
+            //let new_state = check_transitions(state: state, symbol: String(current_symbol))
+           // if new_state != "" {
+            //  next.append(new_state)
+            //}
         }
+        current_state=next
       }
+        
 
-      // return states if last state is final
-      if finiteAutomata.finalStates.contains(current_state) {
-        return states
-      }
-      else {
-        return []
-      }
+        // return states if last state is final
+        let output = current_state.filter(finiteAutomata.finalStates.contains)
+        if output.count > 0 {
+            return current_state
+        }
+        else {
+            return []
+        }
     }
     
-    private func check_transitions(state: String, symbol: String) -> String {
-      for transition in finiteAutomata.transitions {
-        if transition.from == state && transition.with == symbol {
-          return transition.to
-        }
-      }
-      return ""
-    }
+//    private func check_transitions(state: String, symbol: String) -> String {
+//      for transition in finiteAutomata.transitions {
+//        if transition.from == state && transition.with == symbol {
+//            return transition.to
+//        }
+//      }
+ //     return ""
+//    }
+    
 }
